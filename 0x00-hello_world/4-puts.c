4dad386b469a15b4145e42b6c3863b8c0e2733a4
ololadeayoms <ololadeobems@gmail.com>
2022-09-08 07:10:51 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,40 @@
-#include <stdio.h>
+#include <stdio.h>                                                                                               
 
-/**
-* main - print the string in the put function
-*
-* Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle
-* Return: 0
-*/
-int main(void)
-{
-       puts("\"Programming is like building a multilingual puzzle");
-       return (0);
-)
+                                                                                                                 
+
+/**                                                                                                              
+
+* main - print the string in the function                                                                        
+
+*                                                                                                                
+
+* Description: using the main function                                                                           
+
+* this program prin#include <stdio.h>                                                                                               
+
+                                                                                                                 
+
+/**                                                                                                              
+
+* main - print the string in the function                                                                        
+
+*                                                                                                                
+
+* Description: using the main function                                                                           
+
+* this program prints "Programming is like building a multilingual puzzle                                        
+
+* Return: 0                                                                                                      
+
+*/                                                                                                               
+
+int main(void)                                                                                                   
+
+{                                                                                                                
+
+       puts("\"Programming is like building a multilingual puzzle");                                             
+
+       return (0);                                                                                               
+
+}                                                                                                                
 
