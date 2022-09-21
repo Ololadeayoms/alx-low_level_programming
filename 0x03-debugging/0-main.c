02481682efc36970fbf846cfae27dc145f6d74d9
ololadeayoms <ololadeobems@gmail.com>
2022-09-21 01:15:37 -0700

    0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
+
 /**
- *main - prints positive or negativr integer
- *Return: 0
+ *main - prints positive or negative integer
+ *Return : 0
  */
 
 int main(void)
 
 {
-	int i;
-	i = 0;
-	positive or negative(i);
-	return (0);
-}
 
+  int i;
+  i = 0
+    positive_or_negative(i);
+  return(0);
+}
